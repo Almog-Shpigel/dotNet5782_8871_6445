f247f9ed3088155ed1faff3e8057ca05c9294ad7
almog <shpigel@g.jct.ac.il>
2021-10-04 11:24:27 +0300

    Initial code + Welcome functions

---
+++ dotNet5782_8871_6445/Targil0/Program8871.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8871();
+            Welcome6445();
+            Console.ReadKey();
+
+        }
+        static partial void Welcome6445();
+        private static void Welcome8871()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}

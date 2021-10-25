9cf2d827d1b8776ca8f5d2d9fe3622bc64fd0a0c
Amitai Salomon <amitaisal@gmail.com>
2021-10-25 20:46:38 +0300

    Commit All

--- dotnet5782_8871_6445/DAL/Station.cs
+++ dotnet5782_8871_6445/DAL/Station.cs
@@ -23,28 +23,28 @@ namespace IDAL
                 Longitude = longitude;
                 Latitude = latitude;
             }
-            public void print()
+            public override string ToString()
             {
-                string sLong = ConvertCoordinates(Longitude), sLatit = ConvertCoordinates(Latitude);    // Converts the coordinates to be in base 60 (bonus).
-                System.Console.WriteLine($"{Name} #{ID}:\n" +
+                string sLong = ConvertCoordinates(Longitude), sLatit = ConvertCoordinates(Latitude);    /// Converts the coordinates to be in base 60 (bonus).
+                return ($"{Name} #{ID}:\n" +
                     $"Charge slots available: {ChargeSlots}\n" +
-                    $"Location: (" + sLong + ", " + sLatit + ")\n");
+                    $"Location: (" + sLong + "E, " + sLatit + "N)\n");
             }
             public string ConvertCoordinates(double number)
             {
                 int result, remainder;
                 string coordinates;
                 result = (int)number;
-                coordinates = result + "d ";                            // coordinates holds now the degrees.
+                coordinates = result +""+ (char)176 + " " ;                            /// coordinates holds now the degrees.
                 result = (int)((number - result) * 10000);
                 result = result * 60;
                 remainder = result % 10000;
                 result /= 10000;
-                coordinates += result + "\' ";                          // coordinates holds now the minutes.
+                coordinates += result + "\' ";                          /// coordinates holds now the minutes.
                 result = remainder * 60;
                 remainder = result % 10000;
                 result /= 10000;
-                coordinates += result + "." + remainder + "\'\' ";      // coordinates holds now the seconds.
+                coordinates += result + "." + remainder + "\" ";      /// coordinates holds now the seconds.
                 return coordinates;
             }
         }

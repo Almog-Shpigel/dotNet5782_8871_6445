cc822999716b07c56f4ebf5602055513c4b68a92
almog <shpigel@g.jct.ac.il>
2021-11-22 18:06:47 +0200

    Added DroneAvailable function

---
+++ dotNet5782_8871_6445/BL/Exceptions/InvalidTimeChargedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace IBL
+{
+    [Serializable]
+    internal class InvalidTimeChargedException : Exception
+    {
+        public InvalidTimeChargedException()
+        {
+        }
+
+        public InvalidTimeChargedException(string message) : base(message)
+        {
+        }
+
+        public InvalidTimeChargedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidTimeChargedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

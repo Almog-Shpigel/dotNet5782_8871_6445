073299dadbdb55b97a6df79ca83c0aa5189d537f
almog <shpigel@g.jct.ac.il>
2021-12-09 12:55:13 +0200

    .

--- dotNet5782_8871_6445/PL/DroneWindow.xaml.cs
+++ dotNet5782_8871_6445/PL/DroneWindow.xaml.cs
@@ -78,7 +78,7 @@ namespace PL
 
         private void EnableButton()
         {
-            if(InvalidDroneIDBlock.Visibility != Visibility.Visible &&
+            if (InvalidDroneIDBlock.Visibility != Visibility.Visible &&
                 EnterDroneIDBox.Text != "" &&
                InvalidStationIDBlock.Visibility != Visibility.Visible &&
                EnterStationIDBox.Text != "" &&

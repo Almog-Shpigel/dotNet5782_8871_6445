050ab9729b54a24cab09b262b32e66e8361cb84e
Amitai Salomon <amitaisal@gmail.com>
2022-01-09 10:36:50 +0200

    Small changes

--- dotNet5782_8871_6445/BL/BO/Location.cs
+++ dotNet5782_8871_6445/BL/BO/Location.cs
@@ -4,28 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    namespace BO
+namespace BO
+{
+    public class Location
     {
-        public class Location
+        public double Latitude;
+        public double Longitude;
+        public Location(double latitude = 0, double longitude = 0) { Latitude = latitude; Longitude = longitude; }
+        public override string ToString()
         {
-            public double Latitude;
-            public double Longitude;
-            public Location(double latitude = 0, double longitude = 0) { Latitude = latitude; Longitude = longitude; }
-            public override string ToString()
-            {
-                string sLatit = ConvertCoordinates(Latitude), sLong = ConvertCoordinates(Longitude);    /// Converts the coordinates to be in base 60 (bonus).
-                return sLatit + "N, " + sLong + "E";
-            }
-            public string ConvertCoordinates(double number)
-            {
-                //string coordinates;
-                int sec = (int)Math.Round(number * 3600);
-                int deg = sec / 3600;
-                sec = Math.Abs(sec % 3600);
-                int min = sec / 60;
-                sec %= 60;
-                return deg + "" + (char)176 + " " + min + " " + sec;
-            }
+            string sLatit = ConvertCoordinates(Latitude), sLong = ConvertCoordinates(Longitude);    /// Converts the coordinates to be in base 60 (bonus).
+            return sLatit + "N, " + sLong + "E";
+        }
+        public string ConvertCoordinates(double number)
+        {
+            //string coordinates;
+            int sec = (int)Math.Round(number * 3600);
+            int deg = sec / 3600;
+            sec = Math.Abs(sec % 3600);
+            int min = sec / 60;
+            sec %= 60;
+            return deg + "" + (char)176 + " " + min + " " + sec;
         }
     }
+}
 
